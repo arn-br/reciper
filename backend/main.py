076b20848167122de61bc92c0365932f162cfd68
arn-br <arin.barak@wbd.com>
2025-10-07 15:49:15 -0400

    added starlette mounting for frontend

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,22 @@ from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 from schema import schema
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="../frontend/dist", html=True), name="static")
 graphql_app = GraphQLRouter(schema)
 
 app.include_router(graphql_app, prefix="/graphql")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
+        "http://localhost:3000",  # Vite dev server
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
